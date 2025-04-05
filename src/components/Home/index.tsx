e9bd9a8606d6370e5c1a25b18b0e87c0ab0a1df9
Dinh Quan <quanvonguyendinh@gmail.com>
2025-04-05 14:35:15 +0700

    handle home ui

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,5 +1,14 @@
 const Home = () => {
-  return <section>Home</section>;
+  return (
+    <section id="home" className="h-[512px] flex justify-center items-center">
+      <div className="relative inline-block select-none">
+        <span className="text-9xl text-center font-medium">portfolio.</span>
+        <span className="absolute -bottom-4 right-0 text-lg">
+          VO NGUYEN DINH QUAN
+        </span>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
