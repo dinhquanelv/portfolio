b9ff30a77e4481f26706b6c17a1c7217f61d6688
Dinh Quan <quanvonguyendinh@gmail.com>
2025-04-05 13:06:18 +0700

    set up page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import Header from '@/components/Header';
+import Home from '@/components/Home';
+import AboutMe from '@/components/AboutMe';
+import Skills from '@/components/Skills';
+import Projects from '@/components/Projects';
+import Contact from '@/components/Contact';
+import Footer from '@/components/Footer';
+
 function App() {
-  return <>cc</>;
+  return (
+    <div className="m-auto flex max-w-[1440px] flex-col items-center justify-center px-16">
+      <Header />
+      <main className="h-[1000px]">
+        <Home />
+        <AboutMe />
+        <Skills />
+        <Projects />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
