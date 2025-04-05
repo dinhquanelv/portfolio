a9e176447f4fcaf94d493d63b94137edc52b9afa
Dinh Quan <quanvonguyendinh@gmail.com>
2025-04-05 11:11:58 +0700

    config shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
 import { JSX } from 'react';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
 
 function App(): JSX.Element {
-  return <h1 className="text-center p-5">portfolio</h1>;
+  return (
+    <>
+      <h1 className="text-center p-5">portfolio</h1>
+      <Sheet>
+        <SheetTrigger>Open</SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </>
+  );
 }
 
 export default App;
