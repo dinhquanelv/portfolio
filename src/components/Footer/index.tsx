8c901614e73f491c4f85362c261bc1bcd362706b
Dinh Quan <quanvonguyendinh@gmail.com>
2025-04-22 17:06:23 +0700

    fix: remove mail in footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,6 @@
 import SocialIcon from './SocialIcon';
 import facebook from '@/assets/facebook.png';
 import github from '@/assets/github.png';
-import gmail from '@/assets/gmail.png';
 import instagram from '@/assets/instagram.png';
 import linkedin from '@/assets/linkedin.png';
 import youtube from '@/assets/youtube.png';
@@ -12,7 +11,6 @@ const Footer = () => {
       <div className="flex flex-row justify-center items-center mb-12">
         <SocialIcon icon={facebook} link="https://www.facebook.com/dinhquanelv" />
         <SocialIcon icon={github} link="https://github.com/DinhQuanELV" />
-        <SocialIcon icon={gmail} link="mailto:quanvonguyendinh@gmail.com" />
         <SocialIcon icon={instagram} link="https://www.instagram.com/dinhquanelv/" />
         <SocialIcon icon={linkedin} link="https://www.linkedin.com/in/dinhquanelv/" />
         <SocialIcon icon={youtube} link="https://www.youtube.com/@DinhQuanELV" />
