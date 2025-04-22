f921fdd31b6a7d120459f1d4abf0129249f0e45a
Dinh Quan <quanvonguyendinh@gmail.com>
2025-04-22 14:39:33 +0700

    feat: implement projects UI

--- src/components/Projects/Project.tsx
+++ src/components/Projects/Project.tsx
@@ -1,16 +1,31 @@
 interface ProjectProps {
   title: string;
+  link: string;
   description: string;
   thumbnail: string;
 }
 
-const Project = ({ title, description, thumbnail }: ProjectProps) => {
+const Project = ({ title, link, description, thumbnail }: ProjectProps) => {
   return (
-    <div className="w-full md:w-[46%] lg:w-[30%] aspect-video rounded-3xl bg-slate-600">
-      <img src={thumbnail} alt="thumbnail" />
-      <div className="p-2">
-        <span>{description}</span>
-        <span>{title}</span>
+    <div className="relative w-[95%] md:w-[46%] lg:w-[30%] aspect-video rounded-3xl overflow-hidden bg-slate-600 group">
+      <img
+        src={thumbnail}
+        alt="thumbnail"
+        className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+      />
+      <div
+        className="absolute inset-0 flex flex-col justify-between items-center p-4 xl:p-8
+      text-whiteText bg-[#002f5f] opacity-0 group-hover:opacity-100 transition-opacity duration-500"
+      >
+        <p className="text-center text-lg font-medium">{description}</p>
+        <button
+          className="w-36 h-12 p-2 border border-btnHover rounded-full outline-none 
+        font-semibold hover:bg-btnHover transition-colors duration-300"
+        >
+          <a href={link} target="_blank" rel="noopener noreferrer">
+            {title}
+          </a>
+        </button>
       </div>
     </div>
   );
--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,42 +1,49 @@
-import Title from '@/components/Title';
-import Project from './Project';
-
 import { Element } from 'react-scroll';
+
+import Project from './Project';
+import Title from '@/components/Title';
+import test from '@/assets/test.png';
 
 const Projects = () => {
   return (
     <Element name="projects">
       <Title title="projects." description="PROJECTS THAT I MADE" />
-      <div className="flex flex-wrap justify-center gap-8">
+      <div className="flex flex-wrap justify-center gap-6">
         <Project
-          title="Visit website"
-          description="lores lores lores lores lores lores lores lores lores lores lores lores lores"
-          thumbnail=""
+          title="Coming Soon ..."
+          link=""
+          description="E-Commerce platform built with NextJS, NestJS, MySQL and Recommendation Systems"
+          thumbnail={test}
         />
         <Project
-          title="Visit website"
-          description="lores lores lores lores lores lores lores lores lores lores lores lores lores"
-          thumbnail=""
+          title="Visit Website >"
+          link="https://github.com/DinhQuanELV/SocialNetwork"
+          description="Social Media platform built with ReactJS, ExpressJS, MongoDB and Web Socket"
+          thumbnail={test}
         />
         <Project
-          title="Visit website"
-          description="lores lores lores lores lores lores lores lores lores lores lores lores lores"
-          thumbnail=""
+          title="Visit Website >"
+          link="https://github.com/DinhQuanELV/StudentGradeManagement"
+          description="Student Grade Management platform built with HTML, CSS, PHP, MySQL"
+          thumbnail={test}
         />
         <Project
-          title="Visit website"
-          description="lores lores lores lores lores lores lores lores lores lores lores lores lores"
-          thumbnail=""
+          title="Play game >"
+          link="https://dinhquanelv.github.io/FlappyPigMobile/"
+          description="Flappy Pig game on Mobile web built with HTML, CSS, JavaScript"
+          thumbnail={test}
         />
         <Project
-          title="Visit website"
-          description="lores lores lores lores lores lores lores lores lores lores lores lores lores"
-          thumbnail=""
+          title="View Code >"
+          link="https://github.com/DinhQuanELV/IEEE754"
+          description="IEEE 754 Floating Point Converter built with C++"
+          thumbnail={test}
         />
         <Project
-          title="Visit website"
-          description="lores lores lores lores lores lores lores lores lores lores lores lores lores"
-          thumbnail=""
+          title="View Code >"
+          link="https://github.com/DinhQuanELV/EmployeeManage"
+          description="Employee manage console application build with C++"
+          thumbnail={test}
         />
       </div>
     </Element>
